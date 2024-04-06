41df0f5bfe5479728c550b0302c0e073bf0d2fce
Mariyam Iqbal <[valid-email]>
2024-04-06 11:03:05 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+
+const currency:  any = {
+    USD: 1,   //base currency
+    PKR: 280,
+    INR: 74.57,
+    EUR: 0.91,
+    GBP: 0.76,
+
+};
+
+let user_Answer = await inquirer.prompt(
+[
+    {
+    name: "from",
+    message: "Enter From Currency",
+    type: "list",
+    choices: ["USD","PKR","INR","EUR","GBP"]
+    },
+    {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD","PKR","INR","EUR","GBP"]
+    },
+    {  
+    name: "Amount",
+    message: "Enter amount",
+    type: "number" 
+    }
+  ]
+)
+
+let fromAmount = currency[user_Answer.from]
+let toAmount = currency[user_Answer.to]
+let amount = user_Answer.Amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+
+
+
+
